313268eb6b1f5f585018fdf9c7bc51f7a3e4d4d0
SergioWesker <wesker06.91@gmail.com>
2014-09-20 20:27:37 -0500

    index
    El index de la practica de mvc

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	switch($_REQUEST['controlador'])
+	{
+		case 'vehiculos':
+		//Carga el controlador
+		require('controlador/vehiculoCtl.php');
+		$controlador = new VehiculoCtl();
+		Break;
+	}
+	
+	//Ejecuta el controlador
+	$controlador->ejecutar();
+?>
