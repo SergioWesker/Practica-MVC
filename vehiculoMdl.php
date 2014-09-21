4fc6551e25a2409731393e75ce065f5c5fcd228c
SergioWesker <wesker06.91@gmail.com>
2014-09-20 20:30:24 -0500

    Modelo
    el modelo de la practica de mvc

---
+++ vehiculoMdl.php
@@ -0,0 +1,12 @@
+<?php
+	class vehiculoMdl
+	{
+		public function alta($VIN,$KM,$Combustible,$Golpes,$Ubicacion)
+		{
+			$vehiculo = $VIN,$KM,$Combustible,$Golpes,$Ubicacion;
+			
+			return $vehiculo;
+		}
+		
+	}
+?>
